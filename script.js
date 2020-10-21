6300a1b9839f0c359b458bfcb471ada7e7be8dda
DenisRybkin <72266873+DenisRybkin@users.noreply.github.com>
2020-10-21 23:19:42 +0300

    calculator 2.0

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
-function insert(num) {
-  document.querySelector('.output').value = document.querySelector('.output').value+num;
-}
 
-function equal() {
-  let expresion = document.querySelector('.output').value;
 
-  if(expresion) {
-    document.querySelector('.output').value = eval(document.querySelector('.output').value);
-  }
-}
+  function insert(num) {
+    document.querySelector('.output').value = document.querySelector('.output').value+num;
+   }
+   function insertOutput_2(act) {
+     const output = document.querySelector('.output');
+     const output_2 = document.querySelector('.output_2');
 
-function clean() {
-  document.querySelector('.output').value="";
-}
+     if (output.value != '') {
+       output.value += ` ${act}`;
+       output_2.value += output.value;
+       output.value = '';
+     }
+   }
+
+   function CE(){
+    document.querySelector('.output').value="";
+   }
+
+   function equal() {
+     let expresion = document.querySelector('.output').value;
+     let output_2 = document.querySelector('.output_2');
+     let ifEqual = true;
+
+     if(expresion) {
+       document.querySelector('.output').value = eval(output_2.value + document.querySelector('.output').value);
+       output_2.value = '';
+       
+     }
+   }
+
+   function clean() {
+     document.querySelector('.output').value="";
+     document.querySelector('.output_2').value="";
+   }
+   function plusMinus() {
+     const output = document.querySelector('.output');
+
+     if (!!output) {
+       output.value = -output.value;
+     }
+   }
+   function Delete(){
+     let output = document.querySelector('.output');
+     
+     output.value = output.value.slice(0,-1);
+   }
+ 
